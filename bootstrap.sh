71e5755b7778d4d6165036364eb2df5650a70aae
baleen37 <baleen37@gmail.com>
2015-05-04 09:44:23 +0000

    vundle 제대로 올리고, 자동으로 plugin 설치되게

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,9 +4,11 @@ cd "$(dirname "${BASH_SOURCE}")";
 
 git pull origin master;
 
-function doIt() {
-    rsync  --exclude "README.md" --exclude "bootstrap.sh" --exclude ".git/" -arv . ~;
-    source ~/.bash_profile;
-}
+# syncronize config file for initaliztion
+rsync  --exclude "README.md" --exclude "bootstrap.sh" --exclude ".git/" -arv . ~;
+source ~/.bash_profile;
 
-doIt;
+
+# install Vundle
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +BundleInstall +qall #2&> /dev/null
