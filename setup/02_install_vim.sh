f4a065d9c741b956564489d940ad7aa0db364403
baleen37 <baleen37@gmail.com>
2016-07-05 12:07:15 +0900

    Add git config vim path

--- setup/02_install_vim.sh
+++ setup/02_install_vim.sh
@@ -6,4 +6,7 @@ brew install macvim --override-system-vim
 git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
 cd ~/.vim/bundle/YouCompleteMe
 git submodule update --init --recursive
-./install.sh --clang-completer
+./install.sh --clang-completer --tern-completer
+
+# Add git config vim path
+git config --global core.editor `which vim`
