8e446e186e793a61f75ae935042ded8ce85ff5b4
Jiho Lee <baleen37@gmail.com>
2023-09-17 14:15:08 +0900

    chore: resetup

--- install.sh
+++ install.sh
@@ -1,30 +1,24 @@
-#/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
 
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # ohmyzsh
+echo "Setting up ohmyzsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Bunch of symlinks
-ln -sfv "$DOTFILES_DIR/.zshrc" ~
-ln -sfv "$DOTFILES_DIR/.tmux.conf" ~
-ln -sfv "$DOTFILES_DIR/.gitconfig" ~
-ln -sfv "$DOTFILES_DIR/.gitignore_global" ~
-ln -sfv "$DOTFILES_DIR/.aliases" ~
-ln -sfv "$DOTFILES_DIR/.vimrc" ~
-ln -sfv "$DOTFILES_DIR/.ideavimrc" ~
-ln -sfv "$DOTFILES_DIR/.ctags.d" ~
-ln -sfv "$DOTFILES_DIR/bin" ~
-ln -sfv "$DOTFILES_DIR/.config/nvim" ~/.config/nvim
-ln -s "~/Dropbox/wiki" ~
+. "$DOTFILES_DIR/setup.sh"
 
+echo "Setting up vim"
 . "$DOTFILES_DIR/install-vim.sh"
 
 # Package managers & packages
+echo "Setting up package managers"
 . "$DOTFILES_DIR/utils/brew.sh"
 . "$DOTFILES_DIR/utils/misc.sh"
-. "$DOTFILES_DIR/utils/osx.sh"
 
 if [ "$(uname)" == "Darwin" ]; then
+  . "$DOTFILES_DIR/utils/osx.sh"
   . "$DOTFILES_DIR/utils/brew-cask.sh"
 fi
