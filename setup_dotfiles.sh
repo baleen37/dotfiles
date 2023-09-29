ddef06484009f635ff3703287775f4568bb6fc26
Jiho Lee <baleen37@gmail.com>
2023-09-29 11:29:38 +0900

    chore: add

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -2,14 +2,14 @@
 set -euo pipefail
 
 echo "Setting up symlinks"
-ln -sfv "$DOTFILES_DIR/.zshrc" ~
-ln -sfv "$DOTFILES_DIR/.tmux.conf" ~
-ln -sfv "$DOTFILES_DIR/.gitconfig" ~
-ln -sfv "$DOTFILES_DIR/.gitignore_global" ~
-ln -sfv "$DOTFILES_DIR/.aliases" ~
-ln -sfv "$DOTFILES_DIR/.vimrc" ~
-ln -sfv "$DOTFILES_DIR/.ideavimrc" ~
-ln -sfv "$DOTFILES_DIR/.ctags.d" ~
-ln -sfv "$DOTFILES_DIR/bin" ~
-ln -sfv "$DOTFILES_DIR/.config/nvim" ~/.config/nvim
+ln -sfv ".zshrc" ~
+ln -sfv ".tmux.conf" ~
+ln -sfv ".gitconfig" ~
+ln -sfv ".gitignore_global" ~
+ln -sfv ".aliases" ~
+ln -sfv ".vimrc" ~
+ln -sfv ".ideavimrc" ~
+ln -sfv ".ctags.d" ~
+ln -sfv "bin" ~
+ln -sfv ".config/nvim" ~/.config/nvim
 ln -sfv "~/Library/Mobile Documents/iCloud~md~obsidian/Documents/wiki" ~
