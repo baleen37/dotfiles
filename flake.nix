19aab0a168785d8b536076a044070cb53e9fc5be
Jiho Lee <baleen37@gmail.com>
2025-05-05 12:21:17 +0900

    chore: git 추가

--- flake.nix
+++ flake.nix
@@ -4,20 +4,25 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs: {
-    defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
-    defaultPackage.x86_64-darwin = home-manager.defaultPackage.x86_64-darwin;
- 
-    homeConfigurations = {
-      "baleen" = inputs.home-manager.lib.homeManagerConfiguration {
-        system = "x86_64-darwin"; # TODO: replace with x86_64-linux on Linux
-        homeDirectory = "/Users/baleen"; # TODO: make this match your home directory
-        username = "baleen"; # TODO: Change to your username
-        configuration.imports = [ ./home.nix ];
-      };
+  outputs = { nixpkgs, home-manager, ... }:
+  let
+    system = "aarch64-darwin";
+  in {
+    packages.${system}.default = home-manager.defaultPackage.${system};
+
+    homeConfigurations."baleen" = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.${system};
+      modules = [ 
+        ./home.nix
+        {
+          home = {
+            username = "baleen";
+            homeDirectory = "/Users/baleen";
+          };
+        }
+      ];
     };
   };
 }
--- home.nix
+++ home.nix
@@ -1,8 +1,15 @@
-{ ... }: {
+{ pkgs, ... }: {
     programs.home-manager.enable = true;
 
     # Packages to install
     home.packages = with pkgs; [
-        cowsay
-    ]
+      git
+    ];
+
+    # Install the gitconfig file, as .gitconfig in the home directory
+    home.file.".gitconfig".source = ./.gitconfig;
+
+    # Required field - add stateVersion
+    home.stateVersion = "23.11"; # 사용 중인 Nix/Home-Manager 버전에 맞게 조정할 수 있습니다
 }
+
