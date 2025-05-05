ddddb18e3ddf621200500945c7d3341aaafb287b
Jiho Lee <baleen37@gmail.com>
2025-05-05 16:55:19 +0900

    chore

--- flake.nix
+++ flake.nix
@@ -2,67 +2,51 @@
   description = "Home Manager configuration of baleen";
 
   inputs = {
-    # Specify the source of Home Manager and Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nix-darwin = {
+      url = "github:nix-darwin/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }:
+  outputs = {
+      self,
+      nixpkgs,
+      nix-darwin,
+      home-manager,
+      ...
+    }@inputs:
     let
-      # Define supported systems
-      supportedSystems = [ "aarch64-darwin" "aarch64-linux" "x86_64-linux" "x86_64-darwin" ];
-      darwinSystems = [ "aarch64-darwin" "x86_64-darwin" ];
+      # dev-shell = import ./libraries/dev-shell { inherit inputs; };
+      home-manager-shared = ./libraries/home-manager;
+      nixpkgs-shared = ./libraries/nixpkgs;
 
-      # Helper function to generate outputs for each system
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      forDarwinSystems = nixpkgs.lib.genAttrs darwinSystems;
-
-      # Import nixpkgs for each system
-      nixpkgsFor = forAllSystems (system: import nixpkgs {
-        inherit system;
-        config = {
-          allowUnfree = true;
-        };
-      });
-    in {
-      # Only Darwin home configuration
-      homeConfigurations."baleen" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgsFor."aarch64-darwin";
-
-        # Use proper path reference using self
+    in
+    {
+      darwinConfigurations.darwin = nix-darwin.lib.darwinSystem {
+        system = "aarch64-darwin";
         modules = [
-          (import ./modules/darwin/home.nix)
+          # home-manager-shared
+          # nixpkgs-shared
+          home-manager.darwinModules.home-manager
+          # ./modules/shared/configuration.nix
+          # ./modules/darwin/configuration.nix
+          # ./modules/darwin/home.nix
         ];
-
-        # Pass extra parameters to home modules
-        extraSpecialArgs = {
-          # Add any special arguments you want to pass to your modules here
-        };
+        specialArgs = { inherit inputs; };
       };
 
-      # Add default packages for each system
-      packages = forAllSystems (system:
-        let
-          isDarwin = builtins.elem system darwinSystems;
-        in
-        if isDarwin then {
-          # For Darwin systems, provide the home configuration
-          default = self.homeConfigurations."baleen".activationPackage;
-        } else {
-          # For non-Darwin systems, provide a dummy package
-          default = nixpkgsFor.${system}.runCommand "unsupported-system" {} ''
-            echo "This system (${system}) is not supported for home configuration"
-            mkdir -p $out/bin
-            echo "echo 'This system is not supported for home configuration'" > $out/bin/unsupported
-            chmod +x $out/bin/unsupported
-          '';
-        }
-      );
-
-      # Add default package shortcuts
-      defaultPackage = forAllSystems (system: self.packages.${system}.default);
+      # nixosConfigurations.linux = nixpkgs.lib.nixosSystem {
+      #   system = "x86_64-linux";
+      #   modules = [
+      #     home-manager.nixosModules.home-manager
+      #   ];
+      #   specialArgs = { inherit inputs; };
+      # };
     };
 }
--- modules/darwin/home.nix
+++ modules/darwin/home.nix
@@ -4,27 +4,31 @@
   inputs,
   ...
 }: {
-    home.username = "baleen"; # 사용자 이름
-    home.homeDirectory = "/Users/baleen"; # 홈 디렉토리 경로
-    programs.home-manager.enable = true;
+    home-manager.useGlobalPkgs = true;
+    home-manager.useUserPackages = true;
+    home-manager.users.baleen =
+        { config, ... }:
+      {
+        home.username = "baleen"; # 사용자 이름
+        home.homeDirectory = "/Users/baleen"; # 홈 디렉토리 경로
 
-    # Packages to install
-    home.packages = with pkgs; [
-        git
-        fzf
+        home.packages = with pkgs; [
+            git
+            fzf
 
-        # devtools
-        jetbrains.datagrip
-        jetbrains.idea-ultimate
-    ];
-    imports = [
-      # ../shared/programs/1password
-      ./programs/raycast
-    ];
+            # devtools
+            jetbrains.datagrip
+            jetbrains.idea-ultimate
+        ];
+        imports = [
+          # ../shared/programs/1password
+          ./programs/raycast
+        ];
 
-    # Install the gitconfig file, as .gitconfig in the home directory
-    home.file.".gitconfig".source = ../../.gitconfig;
+        # Install the gitconfig file, as .gitconfig in the home directory
+        home.file.".gitconfig".source = ../../.gitconfig;
 
-    # Required field - add stateVersion
-    home.stateVersion = "25.05";
+        # Required field - add stateVersion
+        home.stateVersion = "25.05";
+  };
 }
