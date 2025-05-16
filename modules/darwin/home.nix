e525d76bca3882b560d648f9ce0b32d3b1503ad1
Jiho Lee <baleen37@gmail.com>
2025-05-16 16:22:41 +0900

    chore: update

--- modules/darwin/home.nix
+++ modules/darwin/home.nix
@@ -3,48 +3,52 @@
   pkgs,
   inputs,
   ...
-}: {
+}: let
+  commonUserConfig = { username, homeDirectory }: {
+    home.username = username;
+    home.homeDirectory = homeDirectory;
+    home.packages = with pkgs; [
+      git
+      fzf
+      google-chrome
+      brave
+      # devtools
+      jetbrains.datagrip
+      jetbrains.idea-ultimate
+    ];
+    imports = [
+      ../shared/programs/wezterm
+      ../shared/programs/git
+      ../shared/programs/tmux
+      ../shared/programs/nvim
+      ../shared/programs/vscode
+      ../shared/programs/ssh
+      ../shared/programs/act
+      ./programs/raycast
+      ./programs/homerow
+      ./programs/obsidian
+      ./programs/hammerspoon
+      ./programs/karabiner-elements
+      ./programs/syncthing
+    ];
+    home.file.".gitconfig".source = ../../.gitconfig;
+    home.stateVersion = "25.05";
+  };
+in
+{
     home-manager.useGlobalPkgs = true;
     home-manager.useUserPackages = true;
     nixpkgs.config.allowUnfree = true;
 
     home-manager.users.baleen =
-        { config, ... }:
-      {
-        home.username = "baleen"; # 사용자 이름
-        home.homeDirectory = "/Users/baleen"; # 홈 디렉토리 경로
+        { config, ... }: commonUserConfig {
+          username = "baleen";
+          homeDirectory = "/Users/baleen";
+        };
 
-        home.packages = with pkgs; [
-            git
-            fzf
-            google-chrome
-            brave
-
-            # devtools
-            jetbrains.datagrip
-            jetbrains.idea-ultimate
-        ];
-        imports = [
-          ../shared/programs/wezterm
-          ../shared/programs/git
-          ../shared/programs/tmux
-          ../shared/programs/nvim
-          ../shared/programs/vscode
-          ../shared/programs/ssh
-          ../shared/programs/act
-
-          ./programs/raycast
-          ./programs/homerow
-          ./programs/obsidian
-          ./programs/hammerspoon
-          ./programs/karabiner-elements
-          ./programs/syncthing
-        ];
-
-        # Install the gitconfig file, as .gitconfig in the home directory
-        home.file.".gitconfig".source = ../../.gitconfig;
-
-        # Required field - add stateVersion
-        home.stateVersion = "25.05";
-  };
+    home-manager.users.jito =
+        { config, ... }: commonUserConfig {
+          username = "jito";
+          homeDirectory = "/Users/jito";
+        };
 }
