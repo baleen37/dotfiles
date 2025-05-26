316d9fcd174fc980ccb0918d0f386b26980327af
Jiho Lee <baleen37@gmail.com>
2025-05-26 09:56:45 +0900

    chore: update

--- install.sh
+++ install.sh
@@ -9,5 +9,17 @@ else
   echo "Nix is already installed."
 fi
 
-nix --experimental-features 'nix-command flakes' build '.#darwinConfigurations.darwin.system'
-./result/sw/bin/darwin-rebuild switch --flake '.#darwin'
+# Prompt the user for the configuration to install
+echo "Which configuration do you want to install? (baleen or jito):"
+read TARGET_HOST
+
+# Validate input (optional but recommended)
+if [[ "$TARGET_HOST" != "baleen" && "$TARGET_HOST" != "jito" ]]; then
+  echo "Invalid input. Please enter 'baleen' or 'jito'."
+  exit 1
+fi
+
+echo "Selected configuration: $TARGET_HOST"
+
+nix --experimental-features 'nix-command flakes' build ".#darwinConfigurations.$TARGET_HOST.system"
+./result/sw/bin/darwin-rebuild switch --flake ".#$TARGET_HOST"
