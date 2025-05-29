362066923dd30192f0e06c13aa55ef344fca9ad9
Jiho Lee <baleen37@gmail.com>
2025-05-30 00:44:01 +0900

    fix: refactor homerow module to Home Manager style

--- common/modules/user-env/homerow/default.nix
+++ common/modules/user-env/homerow/default.nix
@@ -1,13 +1,30 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}:
+{ config, pkgs, lib, ... }:
 
-{
-  services.homerow = {
+let
+  inherit (pkgs.stdenvNoCC.hostPlatform) isDarwin;
+  homerowPkg = pkgs.callPackage ../../../nix/packages/homerow {};
+
+in
+lib.mkIf isDarwin {
+  assertions = [
+    {
+      assertion = true;
+      message = "Nix homerow only supports darwin.";
+    }
+  ];
+
+  home.packages = [ homerowPkg ];
+
+  launchd.agents.homerow = {
     enable = true;
-
+    config = {
+      ProgramArguments = [
+        "${config.home.homeDirectory}/Applications/Home Manager Apps/${homerowPkg.sourceRoot}/Contents/MacOS/Homerow"
+      ];
+      KeepAlive = true;
+      ProcessType = "Interactive";
+      StandardOutPath = "${config.xdg.cacheHome}/homerow.log";
+      StandardErrorPath = "${config.xdg.cacheHome}/homerow.log";
+    };
   };
 }
