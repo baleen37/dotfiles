5685870740a77e290d7986b99aa1a008305c90fd
Jiho Lee <baleen37@gmail.com>
2025-05-30 00:36:54 +0900

    refactor: move host-specific programs to common/modules/user-env (phip1611 style)

--- common/modules/user-env/homerow/default.nix
+++ common/modules/user-env/homerow/default.nix
@@ -1,45 +1,13 @@
-{ config, pkgs, lib, ... }:
-
-let
-  inherit (pkgs.stdenvNoCC.hostPlatform) isDarwin;
-  cfg = config.services.homerow;
-in
 {
-  options.services.homerow = {
-    enable = lib.mkEnableOption "Homerow background service";
-    package = lib.mkOption {
-      type = lib.types.package;
-      default = pkgs.callPackage ../../../nix/packages/homerow {};
-      description = "Homerow package to use";
-    };
-    config = { };
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+  services.homerow = {
+    enable = true;
+
   };
-
-  config = lib.mkMerge [
-    {
-      assertions = [
-        {
-          assertion = cfg.enable -> isDarwin;
-          message = "Nix homerow only supports darwin.";
-        }
-      ];
-    }
-
-    (lib.mkIf cfg.enable {
-      home.packages = [ cfg.package ];
-
-      launchd.agents.homerow = {
-        enable = true;
-        config = {
-          ProgramArguments = [
-            "${config.home.homeDirectory}/Applications/Home Manager Apps/${cfg.package.sourceRoot}/Contents/MacOS/Homerow"
-          ];
-          KeepAlive = true;
-          ProcessType = "Interactive";
-          StandardOutPath = "${config.xdg.cacheHome}/homerow.log";
-          StandardErrorPath = "${config.xdg.cacheHome}/homerow.log";
-        };
-      };
-    })
-  ];
 }
