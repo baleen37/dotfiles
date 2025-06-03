6656fe205bbbcb0082f786cfc588054c51a674fe
Jiho Lee <baleen37@gmail.com>
2025-06-03 10:21:19 +0900

    chore: init

---
+++ hosts/darwin/default.nix
@@ -0,0 +1,84 @@
+{ config, pkgs, ... }:
+
+let user = "%USER%"; in
+
+{
+  imports = [
+    ../../modules/darwin/home-manager.nix
+    ../../modules/shared
+  ];
+
+  nix = {
+    package = pkgs.nix;
+
+    settings = {
+      trusted-users = [ "@admin" "${user}" ];
+      substituters = [ "https://nix-community.cachix.org" "https://cache.nixos.org" ];
+      trusted-public-keys = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" ];
+    };
+
+    gc = {
+      automatic = true;
+      interval = { Weekday = 0; Hour = 2; Minute = 0; };
+      options = "--delete-older-than 30d";
+    };
+
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+  };
+
+
+  environment.systemPackages = with pkgs; [
+    emacs-unstable
+  ] ++ (import ../../modules/shared/packages.nix { inherit pkgs; });
+
+  launchd.user.agents.emacs.path = [ config.environment.systemPath ];
+  launchd.user.agents.emacs.serviceConfig = {
+    KeepAlive = true;
+    ProgramArguments = [
+      "/bin/sh"
+      "-c"
+      "/bin/wait4path ${pkgs.emacs}/bin/emacs && exec ${pkgs.emacs}/bin/emacs --fg-daemon"
+    ];
+    StandardErrorPath = "/tmp/emacs.err.log";
+    StandardOutPath = "/tmp/emacs.out.log";
+  };
+
+  system = {
+    checks.verifyNixPath = false;
+    primaryUser = user;
+    stateVersion = 4;
+
+    defaults = {
+      NSGlobalDomain = {
+        AppleShowAllExtensions = true;
+        ApplePressAndHoldEnabled = false;
+
+        KeyRepeat = 2; # Values: 120, 90, 60, 30, 12, 6, 2
+        InitialKeyRepeat = 15; # Values: 120, 94, 68, 35, 25, 15
+
+        "com.apple.mouse.tapBehavior" = 1;
+        "com.apple.sound.beep.volume" = 0.0;
+        "com.apple.sound.beep.feedback" = 0;
+      };
+
+      dock = {
+        autohide = false;
+        show-recents = false;
+        launchanim = true;
+        orientation = "bottom";
+        tilesize = 48;
+      };
+
+      finder = {
+        _FXShowPosixPathInTitle = false;
+      };
+
+      trackpad = {
+        Clicking = true;
+        TrackpadThreeFingerDrag = true;
+      };
+    };
+  };
+}
