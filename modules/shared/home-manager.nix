d5e2d0397b41106bf88dd0d83324369976522bd5
Jiho Lee <baleen37@gmail.com>
2025-06-03 16:45:35 +0900

    chore: update

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -282,19 +282,6 @@ let name = "Jiho Lee";
         "/Users/${user}/.ssh/config_external"
       )
     ];
-    matchBlocks = {
-      "github.com" = {
-        identitiesOnly = true;
-        identityFile = [
-          (lib.mkIf pkgs.stdenv.hostPlatform.isLinux
-            "/home/${user}/.ssh/id_github"
-          )
-          (lib.mkIf pkgs.stdenv.hostPlatform.isDarwin
-            "/Users/${user}/.ssh/id_github"
-          )
-        ];
-      };
-    };
   };
 
   tmux = {
