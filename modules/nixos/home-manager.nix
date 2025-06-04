8aeff3b32f3aade182b76d9cccb9f7cf86df2095
jito(지토) <git@baleen.me>
2025-06-04 11:49:58 +0900

    fix: restore CI by removing make steps and unify user

--- modules/nixos/home-manager.nix
+++ modules/nixos/home-manager.nix
@@ -1,7 +1,8 @@
 { config, pkgs, lib, ... }:
 
 let
-  user = "baleen";
+  # The main user for this configuration
+  user = "jito";
   xdg_configHome  = "/home/${user}/.config";
   shared-programs = import ../shared/home-manager.nix { inherit config pkgs lib; };
   shared-files = import ../shared/files.nix { inherit config pkgs; };
