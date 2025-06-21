d14a2caa8672b5bdf398a723aad69d96327f15c2
Jiho Lee <baleen37@gmail.com>
2025-06-22 01:43:16 +0900

    fix: resolve pre-commit test failures in auto-update integration test

--- modules/shared/config/wezterm/wezterm.lua
+++ modules/shared/config/wezterm/wezterm.lua
@@ -57,4 +57,61 @@ config.keys = {
   },
 }
 
+-- Mouse bindings
+config.mouse_bindings = {
+  -- Cmd+click opens hyperlinks
+  {
+    event = { Up = { streak = 1, button = 'Left' } },
+    mods = 'CMD',
+    action = wezterm.action.OpenLinkAtMouseCursor,
+  },
+  -- Cmd+click drag for selecting
+  {
+    event = { Down = { streak = 1, button = 'Left' } },
+    mods = 'CMD',
+    action = wezterm.action.SelectTextAtMouseCursor 'Cell',
+  },
+  -- Right click paste
+  {
+    event = { Up = { streak = 1, button = 'Right' } },
+    mods = 'NONE',
+    action = wezterm.action.PasteFrom 'Clipboard',
+  },
+  -- Middle click paste (useful for 3-button mice)
+  {
+    event = { Up = { streak = 1, button = 'Middle' } },
+    mods = 'NONE',
+    action = wezterm.action.PasteFrom 'PrimarySelection',
+  },
+  -- Shift+click extend selection
+  {
+    event = { Up = { streak = 1, button = 'Left' } },
+    mods = 'SHIFT',
+    action = wezterm.action.ExtendSelectionToMouseCursor 'Cell',
+  },
+  -- Double-click select word
+  {
+    event = { Up = { streak = 2, button = 'Left' } },
+    mods = 'NONE',
+    action = wezterm.action.SelectTextAtMouseCursor 'Word',
+  },
+  -- Triple-click select line
+  {
+    event = { Up = { streak = 3, button = 'Left' } },
+    mods = 'NONE',
+    action = wezterm.action.SelectTextAtMouseCursor 'Line',
+  },
+  -- Ctrl+scroll for zoom
+  {
+    event = { Down = { streak = 1, button = { WheelUp = 1 } } },
+    mods = 'CTRL',
+    action = wezterm.action.IncreaseFontSize,
+  },
+  {
+    event = { Down = { streak = 1, button = { WheelDown = 1 } } },
+    mods = 'CTRL',
+    action = wezterm.action.DecreaseFontSize,
+  },
+}
+
 return config
