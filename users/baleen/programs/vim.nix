5c9042efcb79f0e7bd62c2240eb07806c1374fd9
Jiho Lee <baleen37@gmail.com>
2025-10-25 23:23:38 +0900

    feat(programs): migrate vim configuration
    - Move from modules/shared/programs/vim
    - Convert to programs.neovim with viAlias and vimAlias
    - Preserve all plugins, settings, and extraConfig
    - Maintain bubblegum airline theme and key bindings
    - Add vim/neovim aliases for backward compatibility

---
+++ users/baleen/programs/vim.nix
@@ -0,0 +1,152 @@
+# Vim/Neovim Configuration
+#
+# Complete vim/neovim editor configuration with plugins, key bindings, and display options.
+#
+# Plugins:
+#   - vim-airline: Status bar theme (bubblegum theme, Powerline fonts)
+#   - vim-airline-themes: Additional airline themes
+#   - vim-tmux-navigator: Seamless navigation between tmux panes
+#
+# Main settings:
+#   - Line numbers: Relative line numbers (relativenumber) + current line number
+#   - Search: Incremental search, ignore case
+#   - Tabs/spaces: 2-space indentation, convert tabs to spaces
+#   - Backups: No backup files, swap files in ~/.config/vim/swap
+#   - Clipboard: autoselect mode
+#
+# Key bindings:
+#   - Leader: , (comma)
+#   - LocalLeader: Space
+#   - <Leader>,: Paste from clipboard
+#   - <Leader>.: Copy to clipboard
+#   - <Leader>q: Close window
+#   - Ctrl+h/j/k/l: Move between split windows
+#   - Tab/Shift+Tab: Move between buffers
+#
+# VERSION: 4.0.0 (Migrated to Mitchell style)
+# LAST UPDATED: 2025-10-25
+
+{ pkgs, ... }:
+
+{
+  programs.neovim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
+
+    plugins = with pkgs.vimPlugins; [
+      vim-airline
+      vim-airline-themes
+      vim-tmux-navigator
+    ];
+
+    settings = {
+      ignorecase = true;
+    };
+
+    extraConfig = ''
+      "" General
+      set number
+      set history=1000
+      set nocompatible
+      set modelines=0
+      set encoding=utf-8
+      set scrolloff=3
+      set showmode
+      set showcmd
+      set hidden
+      set wildmenu
+      set wildmode=list:longest
+      set cursorline
+      set ttyfast
+      set nowrap
+      set ruler
+      set backspace=indent,eol,start
+      set laststatus=2
+      set clipboard=autoselect
+
+      " Dir stuff
+      set nobackup
+      set nowritebackup
+      set noswapfile
+      set backupdir=~/.config/vim/backups
+      set directory=~/.config/vim/swap
+
+      " Relative line numbers for easy movement
+      set relativenumber
+      set rnu
+
+      "" Whitespace rules
+      set tabstop=8
+      set shiftwidth=2
+      set softtabstop=2
+      set expandtab
+
+      "" Searching
+      set incsearch
+      set gdefault
+
+      "" Statusbar
+      set nocompatible " Disable vi-compatibility
+      set laststatus=2 " Always show the statusline
+      let g:airline_theme='bubblegum'
+      let g:airline_powerline_fonts = 1
+
+      "" Local keys and such
+      let mapleader=","
+      let maplocalleader=" "
+
+      "" Change cursor on mode
+      :autocmd InsertEnter * set cul
+      :autocmd InsertLeave * set nocul
+
+      "" File-type highlighting and configuration
+      syntax on
+      filetype on
+      filetype plugin on
+      filetype indent on
+
+      "" Paste from clipboard
+      nnoremap <Leader>, "+gP
+
+      "" Copy from clipboard
+      xnoremap <Leader>. "+y
+
+      "" Move cursor by display lines when wrapping
+      nnoremap j gj
+      nnoremap k gk
+
+      "" Map leader-q to quit out of window
+      nnoremap <leader>q :q<cr>
+
+      "" Move around split
+      nnoremap <C-h> <C-w>h
+      nnoremap <C-j> <C-w>j
+      nnoremap <C-k> <C-w>k
+      nnoremap <C-l> <C-w>l
+
+      "" Easier to yank entire line
+      nnoremap Y y$
+
+      "" Move buffers
+      nnoremap <tab> :bnext<cr>
+      nnoremap <S-tab> :bprev<cr>
+
+      "" Like a boss, sudo AFTER opening the file to write
+      cmap w!! w !sudo tee % >/dev/null
+
+      let g:startify_lists = [
+        \ { 'type': 'dir',       'header': ['   Current Directory '. getcwd()] },
+        \ { 'type': 'sessions',  'header': ['   Sessions']       },
+        \ { 'type': 'bookmarks', 'header': ['   Bookmarks']      }
+        \ ]
+
+      let g:startify_bookmarks = [
+        \ '~/.local/share/src',
+        \ ]
+
+      let g:airline_theme='bubblegum'
+      let g:airline_powerline_fonts = 1
+    '';
+  };
+}
