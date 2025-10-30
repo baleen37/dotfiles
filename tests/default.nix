7468914f249681193d85376ff39126c5fb9e5742
Jiho Lee <baleen37@gmail.com>
2025-10-30 22:04:13 +0900

    feat: add automatic test discovery function
    Uses builtins.readDir + lib.pipe pattern from nixpkgs.
    Discovers all *-test.nix files automatically.

--- tests/default.nix
+++ tests/default.nix
@@ -9,6 +9,37 @@ let
   pkgs = import inputs.nixpkgs { inherit system; };
   lib = pkgs.lib;
 
+  # Automatic test discovery function (nixpkgs pattern)
+  # Discovers all *-test.nix files in a directory
+  discoverTests =
+    dir: prefix:
+    lib.pipe (builtins.readDir dir) [
+      # Filter for .nix files, excluding helpers
+      (lib.filterAttrs (
+        name: type:
+        type == "regular"
+        && lib.hasSuffix "-test.nix" name
+        && name != "default.nix"
+        && name != "nixtest-template.nix"
+      ))
+      # Convert filename to test name and import
+      (lib.mapAttrs' (
+        name: _: {
+          name = "${prefix}-${lib.removeSuffix "-test.nix" name}";
+          value = import (dir + "/${name}") {
+            inherit
+              inputs
+              system
+              pkgs
+              lib
+              self
+              ;
+            inherit (nixtest) nixtest;
+          };
+        }
+      ))
+    ];
+
   # Import existing NixTest framework
   nixtest = import ./unit/nixtest-template.nix { inherit pkgs lib; };
 
