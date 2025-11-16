212864834ccdab949acdefee6f4aacad3314c612
Jiho Lee <baleen37@gmail.com>
2025-11-16 10:43:45 +0900

    feat: add modular yabai.lua for Hammerspoon integration
    - Create standalone yabai.lua module based on dmitriiminaev/Hammerspoon-Yabai
    - Update init.lua to use require('yabai') for cleaner code organization
    - Maintains all yabai HyperModal keybindings
    - Uses correct yabai binary path: /run/current-system/sw/bin/yabai
    - Modular approach makes configuration more maintainable

--- users/shared/.config/hammerspoon/init.lua
+++ users/shared/.config/hammerspoon/init.lua
@@ -38,29 +38,8 @@ end)
 HyperModal = spoon.HyperModal
 Hyper:bind({}, 'm', function() HyperModal:toggle() end)
 
--- yabai function for window management
-local yabai = function(args, completion)
- local yabai_output = ""
- local yabai_error = ""
- -- Runs in background very fast
- local yabai_task = hs.task.new("/run/current-system/sw/bin/yabai", function(err, stdout, stderr)
-  print()
- end, function(task, stdout, stderr)
-  if stdout ~= nil then
-   yabai_output = yabai_output .. stdout
-  end
-  if stderr ~= nil then
-   yabai_error = yabai_error .. stderr
-  end
-  return true
- end, args)
- if type(completion) == "function" then
-  yabai_task:setCallback(function()
-   completion(yabai_output, yabai_error)
-  end)
- end
- yabai_task:start()
-end
+-- Load yabai module
+local yabai = require('yabai')
 
 -- HyperModal with yabai bindings
 HyperModal
