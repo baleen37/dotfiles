6b4f498bd4ab709acaa640a555e8f3915542bf9b
Jiho Lee <baleen37@gmail.com>
2026-05-25 12:16:06 +0900

    refactor(packages): rename myHome.packages.media to modules.packages.media

--- users/shared/packages/media.nix
+++ users/shared/packages/media.nix
@@ -5,12 +5,10 @@
   ...
 }:
 let
-  cfg = config.myHome.packages.media;
+  cfg = config.modules.packages.media;
 in
 {
-  options.myHome.packages.media.enable = lib.mkEnableOption "media tools" // {
-    default = true;
-  };
+  options.modules.packages.media.enable = lib.mkEnableOption "media tools";
 
   config = lib.mkIf cfg.enable {
     home.packages = with pkgs; [
