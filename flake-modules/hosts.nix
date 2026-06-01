ee3827d1822c5bef4e69546004764bef34accd93
Jiho Lee <baleen37@gmail.com>
2026-06-01 16:34:00 +0900

    feat(hosts): enable hammerspoon/karabiner on kakaostyle-jito
    hyper+s Slack 바인딩이 이 머신에서도 동작하도록 비활성화 해제

--- flake-modules/hosts.nix
+++ flake-modules/hosts.nix
@@ -15,11 +15,6 @@
       system = "aarch64-darwin";
       class = "darwin";
       user = "jito.hello";
-      homeModules = {
-        # 회사 macOS는 hammerspoon / karabiner를 사용하지 않는다
-        modules.programs.hammerspoon.enable = false;
-        modules.programs.karabiner.enable = false;
-      };
     };
     vm-aarch64-utm = {
       system = "aarch64-linux";
