e855275f33b757c2d9103ee98f8bc051dd99fe72
Jiho Lee <baleen37@gmail.com>
2026-09-24 12:18:45 +0900

    feat(nvim): add Neo-tree sidebar and cleaner UI

--- users/shared/programs/neovim.nix
+++ users/shared/programs/neovim.nix
@@ -19,6 +19,9 @@ in
         pkgs.vimPlugins.vim-airline
         pkgs.vimPlugins.vim-airline-themes
         pkgs.vimPlugins.vim-tmux-navigator
+        pkgs.vimPlugins.neo-tree-nvim
+        pkgs.vimPlugins.nui-nvim
+        pkgs.vimPlugins.plenary-nvim
         {
           plugin = pkgs.vimPlugins.fzf-lua;
           type = "lua";
@@ -31,6 +34,19 @@ in
         vim.g.maplocalleader = ","
         vim.o.ignorecase = true
         vim.cmd([==[${builtins.readFile ./vim-common.vim}]==])
+
+        vim.g.airline_theme = "minimalist"
+        vim.g.airline_powerline_fonts = 0
+        vim.o.showmode = false
+
+        require("neo-tree").setup({
+          hide_root_node = true,
+          window = {
+            width = 32,
+          },
+        })
+
+        vim.keymap.set("n", "<leader>e", "<cmd>Neotree toggle<cr>", { desc = "Toggle file tree" })
 
         vim.keymap.set("n", "<leader>ff", function()
           require("fzf-lua").files()
